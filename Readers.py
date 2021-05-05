53743908ec18fe9242153474a7d754966a635e98
Elise Grosjean <elise.grosjean@upmc.fr>
2021-05-05 19:59:28 +0200

    readers

---
+++ Readers.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+## VTK Reader with BasicTools
+
+## Elise Grosjean
+## 01/2021
+
+
+import numpy as np
+from pathlib import Path
+import os
+
+def Readmesh(meshFileName):
+    """
+    meshFileName : str (name of the VTK mesh file (.vtu))
+    ----------
+    Read a vtu mesh (the fine and coarse meshes)
+    and return BasicToolsUnstructuredMesh Fine and coarse meshes 
+    """  
+
+    assert isinstance(meshFileName, str)
+    suffix = str(Path(meshFileName).suffix)
+    if suffix == ".vtu":  
+        from BasicTools.IO.VtuReader import VtkToMesh as Read
+        from BasicTools.IO.VtuReader import LoadVtuWithVTK 
+
+    else: 
+        raise ("FileName error!")
+
+    mesh = Read(LoadVtuWithVTK(meshFileName))
+    #print(mesh)
+    return mesh
+  
+
+def VTKReadmesh(meshFileName):
+    """
+    meshFileName : str (name of the VTK mesh file (.vtu))
+    ----------
+    return a vtu mesh (the fine and coarse meshes)
+    """ 
+    assert isinstance(meshFileName, str)
+    suffix = str(Path(meshFileName).suffix)
+    if suffix == ".vtu": 
+        from BasicTools.IO.VtuReader import VtkToMesh as Read
+        from BasicTools.IO.VtuReader import LoadVtuWithVTK 
+
+    else: 
+        raise ("FileName error!")
+    mesh = LoadVtuWithVTK(meshFileName)
+    #print(mesh)
+    return mesh
+  
+
+def VTKReadToNp(SolutionName, tmpbaseFile, i):
+    from BasicTools.IO.VtuReader import LoadVtuWithVTK
+    from vtk.numpy_interface import dataset_adapter as dsa
+        
+    data = LoadVtuWithVTK(tmpbaseFile + str(i) + ".vtu")
+        
+    npArray = dsa.WrapDataObject(data).GetPointData().GetArray(SolutionName)
+    return npArray
+
